128986b58d1008e12294a6eaee8775c37fbfa50b
Langa LMJ <47083471+LanMalJR@users.noreply.github.com>
2024-05-01 21:21:18 +0200

    file_storage fl updated all, and added delete & close

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        filtered_by_class = {}
+        if cls:
+            for key, value in FileStorage.__objects.items():
+                if value.__class__ == cls:
+                    filtered_by_class[key] = value
+            return filtered_by_class
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +54,14 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        ''' delete obj from __objects if it is inside or nothing '''
+        if obj:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
+
+    def close(self):
+        '''deserializes the JSON file to objects'''
+        self.reload()
